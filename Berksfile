74269c098528d3e9d3f4b28e7938a6e18ed93770
Tyler Flint <tylerflint@gmail.com>
2012-10-31 10:03:04 -0600

    tinkering with the sources and dependencies; bumping version

--- Berksfile
+++ Berksfile
@@ -1,9 +1,5 @@
-#!/usr/bin/env ruby
-
 site :opscode
 
 metadata
 
-cookbook 'apt'
-cookbook 'build-essential'
 cookbook 'ruby_build', git: 'git://github.com/fnichol/chef-ruby_build.git'
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,9 @@ maintainer_email "tylerflint@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures ruby leveraging ruby_build recipe"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
+
+depends 'apt'
+depends 'build-essential'
+depends 'ruby_build'
 
